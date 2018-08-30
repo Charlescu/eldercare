000846ea3aa943f089897217b5a663f50d83e6db
cuiyuan <1023968712@qq.com>
2018-08-30 09:17:31 +0800

    崔源
    部分跳转调整

--- js/base.js
+++ js/base.js
@@ -19,13 +19,14 @@ document.addEventListener('follow', function() {
     // }
 })
 
-/*//地址单独获取参数
+
 function getUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
     var r = window.location.search.substr(1).match(reg); //匹配目标参数
     if (r != null) return unescape(r[2]);
     return null; //返回参数值
 }
+/*//地址单独获取参数
 //获取可视范围高度
 function getClientHeight() {
     var clientHeight = 0;
