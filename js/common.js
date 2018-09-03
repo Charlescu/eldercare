ce9dfe7dcc024489aab359518963b4111d9b3bc7
cuiyuan <1023968712@qq.com>
2018-09-03 21:52:35 +0800

    崔源
    修改

--- js/common.js
+++ js/common.js
@@ -1,79 +1,85 @@
 window.onload = function() {
 
-        //支付方式图片切换
-        var $pay_methods = $('.sure_item');
-        $pay_methods.click(function() {
-            $(this).attr('src', '../images/sure.png').siblings().attr('src', '../images/sure_1.png')
-        })
+    //支付方式图片切换
+    var $pay_methods = $('.sure_item');
+    $pay_methods.click(function() {
+        $(this).attr('src', '../images/sure.png').siblings().attr('src', '../images/sure_1.png')
+    })
 
-        //计数器，
-        var count_switch = true,
-            $subtract_img = $('.subtract_img'),
-            $add_img = $('.add_img'),
-            $goods_num = $('.join p').text();
-        $add_img.on('click', function() {
-                ++$goods_num;
-                console.log($goods_num);
-                $('.join p').text($goods_num);
-                $('.xiaoji').text('共' + $goods_num + '件商品  小计：￥' + '元');
-                console.log(parseFloat(($('.xiaoji').text())); count_switch = true;
-                });
+    //计数器，
+    var count_switch = true,
+        $subtract_img = $('.subtract_img'),
+        $add_img = $('.add_img'),
+        $goods_num = $('.join p').text();
+    $add_img.on('click', function() {
+        ++$goods_num;
+        console.log($goods_num);
+        $('.join p').text($goods_num);
+        $('.xiaoji').text('共' + $goods_num + '件商品  小计：￥' + parseFloat(($('.goods_money').text().replace(/[^0-9]/ig, ""))) * $goods_num);
+        $('.all_price span').text('￥' + parseFloat(($('.goods_money').text().replace(/[^0-9]/ig, ""))) * $goods_num);
+        console.log();
+        count_switch = true;
+    });
 
-            $subtract_img.on('click', function() {
-                if (count_switch) {
-                    --$goods_num;
-                    if ($goods_num == 0) {
-                        count_switch = false;
-                    }
-                    console.log($goods_num);
-                    $('.join p').text($goods_num);
-                } else {
-                    return;
-                }
-            })
-
-            //日期选择器
-            //datepicker,日期选择器
-            var start_mon, start_day, end_mon, end_day; $('.get_good_time').click(function(event) {
-                var _this = this;
-                weui.datePicker({
-                    start: new Date(),
-                    end: 2030,
-                    defaultValue: [new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate()],
-                    onConfirm: function(result) {
-                        start_mon = parseInt(result[1].lable);
-                        start_day = parseInt(result[2].lable);
-
-
-                    }
-                });
-            }); $('.back_good_time').click(function(event) {
-                var _this = this;
-                weui.datePicker({
-                    start: new Date(),
-                    end: 2030,
-                    defaultValue: [new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate()],
-                    onConfirm: function(result) {
-                        end_mon = parseInt(result[1].lable);
-                        end_day = parseInt(result[2].lable);
-
-
-                    }
-                });
-            });
-            //二级联动
-
-            //加载AJAx
-            document.addEventListener('scroll', function() {
-                if ($(window).scrollTop() >= $(document).height() - $(window).height()) {
-
-                    pull_load();
-                }
-                // if (window.innerHeight + document.body.scrollTop === document.body.clientHeight) {
-                //     // 当页面高度 + 滚动高度 === 文档整体高度时为滚动至底部 执行异步加载方法等等
-                // console.log(1)
-                // }
-            })
-
-
+    $subtract_img.on('click', function() {
+        if (count_switch) {
+            --$goods_num;
+            if ($goods_num == 0) {
+                count_switch = false;
+            }
+            console.log($goods_num);
+            $('.join p').text($goods_num);
+            $('.xiaoji').text('共' + $goods_num + '件商品  小计：￥' + parseFloat(($('.goods_money').text().replace(/[^0-9]/ig, ""))) * $goods_num);
+            $('.all_price span').text('￥' + parseFloat(($('.goods_money').text().replace(/[^0-9]/ig, ""))) * $goods_num);
+        } else {
+            return;
         }
+    })
+
+    //日期选择器
+    //datepicker,日期选择器
+    var start_mon, start_day, end_mon, end_day;
+    $('.get_good_time').click(function(event) {
+        var _this = this;
+        weui.datePicker({
+            start: new Date(),
+            end: 2030,
+            defaultValue: [new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate()],
+            onConfirm: function(result) {
+                start_mon = parseInt(result[1].lable);
+                start_day = parseInt(result[2].lable);
+
+
+            }
+        });
+    });
+    $('.back_good_time').click(function(event) {
+        var _this = this;
+        weui.datePicker({
+            start: new Date(),
+            end: 2030,
+            defaultValue: [new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate()],
+            onConfirm: function(result) {
+                end_mon = parseInt(result[1].lable);
+                end_day = parseInt(result[2].lable);
+
+
+            }
+        });
+    });
+    //二级联动
+
+    //加载AJAx
+    document.addEventListener('scroll', function() {
+        if ($(window).scrollTop() >= $(document).height() - $(window).height()) {
+
+            pull_load();
+        }
+        // if (window.innerHeight + document.body.scrollTop === document.body.clientHeight) {
+        //     // 当页面高度 + 滚动高度 === 文档整体高度时为滚动至底部 执行异步加载方法等等
+        // console.log(1)
+        // }
+    })
+
+
+}
